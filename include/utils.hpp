50b3ee99f34809ea96fb3c3b2822d26300ed19c7
sadsnowcat <sadcatsnow@gmail.com>
2026-08-27 23:08:56 +0800

    add is_suffix for file path matching in source breakpoints

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,6 +8,7 @@ namespace minidbg {
     // 分割字符串
     std::vector<std::string> split(const std::string &s, char delimiter);
     bool is_prefix(const std::string &s, const std::string &of);
+    bool is_suffix(const std::string& s, const std::string& of);
 } // namespace minidbg
 
 #endif
