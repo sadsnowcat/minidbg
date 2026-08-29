b261649d30c4c16c801226dff85552b1f79a6154
sadsnowcat <sadcatsnow@gmail.com>
2026-08-29 11:29:18 +0800

    新测试

---
+++ examples/stacktest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+long long c(){
+    long long cc = 123456;
+    return cc;
+}
+
+
+long long b(){
+    long long bb = 511114;
+    c();
+    return bb;
+}
+
+int main(){
+    long long aa= 114511;
+    b();
+    return 0;
+}
